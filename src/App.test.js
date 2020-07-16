dafec1bb926549fe27d741d99489f95134575537
PauloFurtunatoAlexandre <furtuna.alexandre@gmail.com>
2020-07-16 17:43:35 -0500

    added all tests and also included fetch test with async await

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,60 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import {
+  render,
+  fireEvent,
+  screen,
+  waitFor
+} from "@testing-library/react";
 import App from "./App";
+import ContactForm from "./components/ContactForm";
 
 test("renders App without crashing", () => {
   render(<App />);
+});
+
+test("test the lenght of first name input", () => {
+  const { getByTestId } = render(<ContactForm />);
+
+  let firstName = getByTestId("firstName");
+  expect(firstName.placeholder).toBe("Edd");
+});
+
+test("testing the texts", () => {
+  const { getByText } = render(<ContactForm />);
+
+  getByText(/first name/i);
+  getByText(/last name/i);
+  getByText(/email/i);
+
+  expect(screen.getByRole("button", /submit/));
+});
+
+test("test button by clicking on it", async () => {
+  const { getByTestId, getByText } = render(<ContactForm />);
+
+  const firstName = getByTestId("firstName");
+  fireEvent.change(firstName, {
+    target: { value: "Paulo" },
+  });
+
+  const lastName = getByTestId("lastName");
+  fireEvent.change(lastName, {
+    target: { value: "Alexandre" },
+  });
+
+  const email = getByTestId("email");
+  fireEvent.change(email, {
+    target: { value: "paulo@paulo.com" },
+  });
+
+  const message = getByTestId("message");
+  fireEvent.change(message, {
+    target: { value: "message" },
+  });
+
+  const submitButton = getByTestId("submitButton");
+  await waitFor(() => {
+    fireEvent.click(submitButton);
+  });
 });
